f3a7630604dcda775b420ca391d53e024d24adea
zyw <40329182+zhangyongwang4444@users.noreply.github.com>
2019-07-02 22:03:00 +0800

    按照多重字段排序 (#2)
    按照多重字段排序 (#2)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -2,12 +2,14 @@ package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Point {
+public class Point implements Comparable<Point> {
 
     private final int x;
     private final int y;
+
     // 代表笛卡尔坐标系中的一个点
     public Point(int x, int y) {
         this.x = x;
@@ -51,9 +53,13 @@ public class Point {
         return String.format("(%d,%d)", x, y);
     }
 
+
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points);
+        return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
@@ -64,5 +70,22 @@ public class Point {
                         new Point(2, 1),
                         new Point(2, -1));
         System.out.println(Point.sort(points));
+    }
+
+    @Override
+    public int compareTo(Point that) {
+        if (this.x < that.x) {
+            return -1;
+        } else if (this.x > that.x) {
+            return 1;
+        }
+
+        if (this.y < that.y) {
+            return -1;
+        } else if (this.y > that.y) {
+            return 1;
+        }
+
+        return 0;
     }
 }
