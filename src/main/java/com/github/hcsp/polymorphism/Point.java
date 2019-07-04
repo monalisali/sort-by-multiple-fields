f3eee82486eda1806f0554db7559c36a26a3261b
JayChenFE <chenjiehwy@163.com>
2019-07-04 22:10:19 +0800

    按照多重字段排序 (#5)
    按照多重字段排序 (#5)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -2,12 +2,14 @@ package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Point {
 
     private final int x;
     private final int y;
+
     // 代表笛卡尔坐标系中的一个点
     public Point(int x, int y) {
         this.x = x;
@@ -53,7 +55,10 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        points.sort(Comparator.comparing(Point::getX).thenComparing(Point::getY));
+        return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
