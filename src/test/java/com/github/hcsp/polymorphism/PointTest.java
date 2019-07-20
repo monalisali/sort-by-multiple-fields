215a0f455591e1b5b65904a42c65f756376053cd
Moon <justwe77@163.com>
2019-07-20 20:18:06 +0800

    modify

--- src/test/java/com/github/hcsp/polymorphism/PointTest.java
+++ src/test/java/com/github/hcsp/polymorphism/PointTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 public class PointTest {
     @Test
     public void test1() {
