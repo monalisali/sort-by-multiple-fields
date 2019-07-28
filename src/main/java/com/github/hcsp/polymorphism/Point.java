9a91cbc5169c0f9d914f0eca8c8bac53f81cfc04
shiliyu <34638563+shiliyu@users.noreply.github.com>
2019-07-28 21:27:10 +0800

    sort a collection of points w/ Comparable. (#20)
    sort a collection of points w/ Comparable. (#20)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -2,12 +2,14 @@ package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Point {
+public class Point implements Comparable<Point> {
 
     private final int x;
     private final int y;
+
     // 代表笛卡尔坐标系中的一个点
     public Point(int x, int y) {
         this.x = x;
@@ -53,7 +55,10 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points);
+        return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
@@ -64,5 +69,22 @@ public class Point {
                         new Point(2, 1),
                         new Point(2, -1));
         System.out.println(Point.sort(points));
+    }
+
+    @Override
+    public int compareTo(Point point) {
+        if (this.x < point.x) {
+            return -1;
+        } else if (this.x > point.x) {
+            return 1;
+        }
+
+        if (this.y < point.y) {
+            return -1;
+        } else if (this.y > point.y) {
+            return 1;
+        }
+
+        return 0;
     }
 }
