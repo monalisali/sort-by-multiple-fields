05b02cd63ff7ac24bde58e65cdbc58abb4d5d7c6
kungua <52815012+kungua@users.noreply.github.com>
2019-08-27 17:39:16 +0800

    sort by multiple fields (#36)
    sort by multiple fields (#36)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -2,12 +2,15 @@ package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
-public class Point {
+public class Point implements Comparable<Point> {
 
     private final int x;
     private final int y;
+
     // 代表笛卡尔坐标系中的一个点
     public Point(int x, int y) {
         this.x = x;
@@ -46,6 +49,7 @@ public class Point {
         return result;
     }
 
+
     @Override
     public String toString() {
         return String.format("(%d,%d)", x, y);
@@ -53,7 +57,10 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points);
+        return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
@@ -64,5 +71,22 @@ public class Point {
                         new Point(2, 1),
                         new Point(2, -1));
         System.out.println(Point.sort(points));
+    }
+
+    @Override
+    public int compareTo(Point that) {
+        if (this.x < that.x) {
+            return -1;
+        } else if (this.x > that.x) {
+            return 1;
+        }
+
+        if (this.y < that.y) {
+            return -1;
+        } else if (this.y > that.y) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
