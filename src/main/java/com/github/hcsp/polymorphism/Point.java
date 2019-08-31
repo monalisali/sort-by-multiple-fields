f81fea3862e1b87bfd8bddfb7cfb9da3425f5e8b
magicP <icanmagic@163.com>
2019-08-31 17:41:52 +0800

    按照多重字段进行排序

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -2,9 +2,10 @@ package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Point {
+public class Point implements Comparable<Point>{
 
     private final int x;
     private final int y;
@@ -53,7 +54,20 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points);
+        return points;
+    }
+
+
+    @Override
+    public int compareTo(Point o) {
+        int result = Integer.compare(x, o.x);
+        if (result == 0){
+            result = Integer.compare(y, o.y);
+        }
+        return result;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
