a620ac6d78136e54b2bc50e4927e76ea0a123f6e
fei tang <497045233@qq.com>
2019-12-03 18:07:21 +0800

    多重字段排序 (#74)
    多重字段排序 (#74)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -1,13 +1,16 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Point {
+public class Point implements Comparable<Point> {
 
     private final int x;
     private final int y;
+
     // 代表笛卡尔坐标系中的一个点
     public Point(int x, int y) {
         this.x = x;
@@ -51,9 +54,14 @@ public class Point {
         return String.format("(%d,%d)", x, y);
     }
 
+
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points);
+        return points;
+    }
+
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
@@ -64,5 +72,41 @@ public class Point {
                         new Point(2, 1),
                         new Point(2, -1));
         System.out.println(Point.sort(points));
+    }
+
+    @Override
+    public int compareTo(Point that) {
+        Class cls1 = this.getClass();
+        Class cls2 = that.getClass();
+        Field[] fields1 = cls1.getDeclaredFields();
+        Field[] fields2 = cls2.getDeclaredFields();
+        for (int i = 0; i < fields1.length; i++) {
+            Field f1 = fields1[i];
+            Field f2 = fields2[i];
+            f1.setAccessible(true);
+            f2.setAccessible(true);
+            Object x1 = null;
+            Object x2 = null;
+            try {
+                x1 = f1.get(this);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            try {
+                x2 = f2.get(that);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            Integer x1ToInt = Integer.parseInt(x1.toString());
+            Integer x2ToInt = Integer.parseInt(x2.toString());
+
+            if (x1ToInt < x2ToInt) {
+                return -1;
+            } else if (x1ToInt > x2ToInt) {
+                return 1;
+            }
+
+        }
+        return 0;
     }
 }
