503d326373c94d42b76c5f0a38f8322aa8d05a35
Maricaya <43136257+Maricaya@users.noreply.github.com>
2019-12-10 16:41:58 +0800

    feat: sort by multiple fields (#78)
    feat: sort by multiple fields (#78)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -2,12 +2,14 @@ package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Point {
+public class Point implements Comparable<Point> {
 
     private final int x;
     private final int y;
+
     // 代表笛卡尔坐标系中的一个点
     public Point(int x, int y) {
         this.x = x;
@@ -51,9 +53,26 @@ public class Point {
         return String.format("(%d,%d)", x, y);
     }
 
+    @Override
+    public int compareTo(Point that) {
+        if (this.x > that.x) {
+            return 1;
+        } else if (this.x < that.x) {
+            return -1;
+        } else if (this.y > that.y) {
+            return 1;
+        } else if (this.y < that.y) {
+            return -1;
+        }
+        return 1;
+    }
+
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points);
+        return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
