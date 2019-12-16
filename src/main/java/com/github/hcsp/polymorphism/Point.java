d9ddb54ba8c3362e093c8c1f2daf908516796678
TangsongyuanmingQingHad <49867878+TangsongyuanmingQingHad@users.noreply.github.com>
2019-12-16 09:32:40 +0800

    内部类重写Compare方法 (#81)
    内部类重写Compare方法 (#81)

--- src/main/java/com/github/hcsp/polymorphism/Point.java
+++ src/main/java/com/github/hcsp/polymorphism/Point.java
@@ -2,6 +2,8 @@ package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Point {
@@ -53,7 +55,24 @@ public class Point {
 
     // 按照先x再y，从小到大的顺序排序
     // 例如排序后的结果应该是 (-1, 1) (1, -1) (2, -1) (2, 0) (2, 1)
-    public static List<Point> sort(List<Point> points) {}
+    public static List<Point> sort(List<Point> points) {
+        Collections.sort(points, (o1, o2) -> {
+            if(o1.x > o2.x) {
+                return 1;
+            }else if(o1.x < o2.x){
+                return -1;
+            }
+
+            if(o1.y>o2.y) {
+                return 1;
+            }else if(o1.y < o2.y) {
+                return -1;
+            }
+
+            return 0;
+        });
+        return points;
+    }
 
     public static void main(String[] args) throws IOException {
         List<Point> points =
